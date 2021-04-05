2b09fd88ad8a220b44320abd89e99790cbcec40e
Rajeev <scraptorajeev@gmail.com>
2021-04-05 14:53:29 +0530

    MySQL schema

--- infrastructure/models.py
+++ infrastructure/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import BIGINT, DATE, DECIMAL, VARCHAR, Column, DateTime
+from sqlalchemy import BIGINT, DECIMAL, VARCHAR, Column, DateTime, Date, TEXT
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
@@ -12,8 +13,57 @@ class Snapshots(Base):
     block_number = Column("block_number", BIGINT, nullable=False)
     amount_held_in_cogs = Column(
         "amount_held_in_cogs", DECIMAL(19, 8), nullable=False)
-    snap_shot_date = Column("snap_shot_date", DATE,
-                            server_default=db.func.now())
-    created_at = Column("created_at", DateTime, server_default=db.func.now())
+    snap_shot_date = Column("snap_shot_date", Date, nullable=False)
+    created_at = Column("created_at", DateTime,
+                        server_default=func.current_timestamp(), nullable=False)
     updated_at = Column("updated_at", DateTime,
-                        server_default=db.func.now(), server_onupdate=db.func.now())
+                        server_default=func.current_timestamp(), server_onupdate=func.current_timestamp(), nullable=False)
+
+
+class UserAccessDetails(Base):
+    __tablename__ = "user_access_details"
+    id = Column("id", BIGINT, primary_key=True, autoincrement=True)
+    wallet_address = Column("wallet_address", VARCHAR(
+        50), nullable=False, index=True)
+    email = Column("email", VARCHAR(
+        120), nullable=False)
+    name = Column("name", VARCHAR(
+        60), nullable=False)
+    login_time = Column("login_time", DateTime, nullable=False)
+    created_at = Column("created_at", DateTime,
+                        server_default=func.current_timestamp(), nullable=False)
+    updated_at = Column("updated_at", DateTime,
+                        server_default=func.current_timestamp(), server_onupdate=func.current_timestamp(), nullable=False)
+
+
+class UserComments(Base):
+    __tablename__ = "user_comments"
+    id = Column("id", BIGINT, primary_key=True, autoincrement=True)
+    wallet_address = Column("wallet_address", VARCHAR(
+        50), nullable=False, index=True)
+    comment = Column("comment", TEXT, nullable=False)
+    created_at = Column("created_at", DateTime,
+                        server_default=func.current_timestamp(), nullable=False)
+    updated_at = Column("updated_at", DateTime,
+                        server_default=func.current_timestamp(), server_onupdate=func.current_timestamp(), nullable=False)
+
+
+class TransferInfo(Base):
+    __tablename__ = "transfer_info"
+    id = Column("id", BIGINT, primary_key=True, autoincrement=True)
+    wallet_address = Column("wallet_address", VARCHAR(
+        50), nullable=False, index=True)
+    transfer_time = Column("transfer_time", DateTime, nullable=False)
+
+    transfer_fees = Column("transfer_fees", DECIMAL(19, 8), nullable=False)
+    transfer_transaction = Column(
+        "transfer_transaction", VARCHAR(50), nullable=False)
+    transfer_amount_in_cogs = Column(
+        "transfer_amount_in_cogs", DECIMAL(19, 8), nullable=False)
+    transfer_status = Column(
+        "transfer_status", VARCHAR(50), nullable=False)
+
+    created_at = Column("created_at", DateTime,
+                        server_default=func.current_timestamp(), nullable=False)
+    updated_at = Column("updated_at", DateTime,
+                        server_default=func.current_timestamp(), server_onupdate=func.current_timestamp(), nullable=False)
---
+++ infrastructure/repository/base_repository.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from common import DATABASE
+
+engine = create_engine(
+    f"{DATABASE['DB_DRIVER']}://{DATABASE['DB_USER']}:"
+    f"{DATABASE['DB_PASSWORD']}"
+    f"@{DATABASE['DB_HOST']}:"
+    f"{DATABASE['DB_PORT']}/{DATABASE['DB_NAME']}", echo=False)
+
+Session = sessionmaker(bind=engine)
+default_session = Session()
+
+
+class BaseRepository:
+    def __init__(self):
+        self.session = default_session
+
+    def add_item(self, item):
+        try:
+            self.session.add(item)
+            self.session.commit()
+        except Exception as e:
+            self.session.rollback()
+            raise e
+
+    def add_all_items(self, items):
+        try:
+            self.session.add_all(items)
+            self.session.commit()
+        except Exception as e:
+            self.session.commit()
+            raise e
