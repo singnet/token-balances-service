66599e2a2ddbda594b4ac5e8735727ba159b18ee
Rajeev <scraptorajeev@gmail.com>
2021-04-06 15:48:25 +0530

    Update config.py

--- common/config.py
+++ common/config.py
@@ -1,8 +1,8 @@
 DATABASE = {
-    "DB_DRIVER": "mysql+pymysql",
+    "DB_DRIVER": "",
     "DB_HOST": "",
     "DB_USER": "",
     "DB_PASSWORD": "",
     "DB_NAME": "",
-    "DB_PORT": 3306
+    "DB_PORT": "",
 }
