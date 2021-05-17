6b127fec6ee9a2b1425dac64cc42d3e026aa4f29
Rajeev <scraptorajeev@gmail.com>
2021-05-17 10:21:30 +0530

    API Service for user registration for AGIX Airdrop

--- application/handlers/customer_handler.py
+++ application/handlers/customer_handler.py
@@ -2,7 +2,7 @@ import json
 
 from utils.format_response import format_response
 from jsonschema import validate, ValidationError, FormatChecker
-from application.services.customer_service import submit_user_question
+from application.services.customer_service import submit_user_question, register_user
 from http import HTTPStatus
 
 
@@ -31,6 +31,35 @@ def submit_question(event, context):
             payload = json.loads(inputs)
             validate(instance=payload, schema=schema, format_checker=FormatChecker())
             statusCode, message = submit_user_question(payload, signature)
+    except ValidationError as e:
+        message = e.message
+
+    return format_response(statusCode, message)
+
+
+def register(event, context):
+    statusCode = HTTPStatus.BAD_REQUEST.value
+
+    schema = {
+        "type": "object",
+        "properties": {
+            "email": {"type": "string", "format": "email"},
+            "wallet_address": {"type": "string"},
+            "block_number": {"type": "number"},
+        },
+        "required": ["wallet_address", "block_number"],
+    }
+
+    try:
+        inputs = event["body"] or None
+        headers = event["headers"]
+        signature = headers.get("Authorization") or None
+        if inputs is None or signature is None:
+            message = HTTPStatus.BAD_REQUEST.phrase
+        else:
+            payload = json.loads(inputs)
+            validate(instance=payload, schema=schema, format_checker=FormatChecker())
+            statusCode, message = register_user(payload, signature)
     except ValidationError as e:
         message = e.message
 
--- testcases/functionaltestcases/test_customer_handler.py
+++ testcases/functionaltestcases/test_customer_handler.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from http import HTTPStatus
-from application.handlers.customer_handler import submit_question
+from application.handlers.customer_handler import submit_question, register
 
 
 class TestCustomerHandler(TestCase):
@@ -25,7 +25,7 @@ class TestCustomerHandler(TestCase):
             "body": '{"wallet_address": "0x176133a958449C28930970989dB5fFFbEdd9F447","email": "user@mail.com","comment": "hello world","block_number": "12333949"}',
             "headers": {
                 "Authorization": "0x36980e7e7ccfe437a5b60ccf26221b2b9ff6f80f39548162d7764c7e7a0992256c42670244b21f5d367420c656aef86e9a164944f10d4f6e64d303c8b575a0d71b"}},
-                                   context=None)
+            context=None)
         self.assertTrue(response['statusCode'] == HTTPStatus.BAD_REQUEST.value)
 
     def test_submit_question_with_block_number_as_number(self):
@@ -33,5 +33,19 @@ class TestCustomerHandler(TestCase):
             "body": '{"wallet_address": "0x176133a958449C28930970989dB5fFFbEdd9F447","email": "user@mail.com","comment": "hello world","block_number": 12333949}',
             "headers": {
                 "Authorization": "0x36980e7e7ccfe437a5b60ccf26221b2b9ff6f80f39548162d7764c7e7a0992256c42670244b21f5d367420c656aef86e9a164944f10d4f6e64d303c8b575a0d71b"}},
-                                   context=None)
+            context=None)
         self.assertTrue(response['statusCode'] == HTTPStatus.OK.value)
+
+    def test_create_user_with_invalid_request(self):
+        response = register(
+            event={"body": '{"wallet_address": "0x12345", "block_number": "0x12345", "email": "mail@mail.com"}',
+                   "headers": {}}, context=None)
+        self.assert_(response['statusCode'] == HTTPStatus.BAD_REQUEST.value)
+
+    def test_create_user_with_request(self):
+        response = register(
+            event={"body": '{"wallet_address": "0x12345", "block_number": "0x12345", "email": "mail@mail.com"}',
+                   "headers": {
+                       "Authorization": "0x36980e7e7ccfe437a5b60ccf26221b2b9ff6f80f39548162d7764c7e7a0992256c42670244b21f5d367420c656aef86e9a164944f10d4f6e64d303c8b575a0d71b"}},
+            context=None)
+        self.assert_(response['statusCode'] == HTTPStatus.BAD_REQUEST.value)
