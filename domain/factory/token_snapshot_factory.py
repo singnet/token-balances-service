a67eade71beeba7651841814d77b3264d7cec83c
Rajeev <scraptorajeev@gmail.com>
2021-05-30 16:19:33 +0530

    Revert "Service update on staked all their tokens and has 0 wallet balance"
    This reverts commit a7581abdd095f95fde0976841523c010a71cf451.

--- domain/factory/token_snapshot_factory.py
+++ domain/factory/token_snapshot_factory.py
@@ -4,7 +4,10 @@ from domain.models.token_snapshot import TokenSnapshot
 class TokenSnapshotFactory:
     def convert_token_snapshot_db_to_entity_model(snapshot, token_transfer_details, staker_transfer_details):
         return TokenSnapshot(
-            snapshot,
+            snapshot.address,
+            snapshot.balance_in_cogs,
+            snapshot.block_number,
+            snapshot.snapshot_date,
             token_transfer_details,
             staker_transfer_details
         )
